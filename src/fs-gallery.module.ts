d53be916089296aa73bd74a0172a4110af1fac08
Anatoly <mendor1990@gmail.com>
2018-09-26 12:13:10 +0300

    initial commit

---
+++ src/fs-gallery.module.ts
@@ -0,0 +1,29 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FsComponentComponent } from './components/fs-component/fs-component.component';
+// import { FsComponentService } from './services';
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    FsComponentComponent,
+  ],
+  entryComponents: [
+  ],
+  declarations: [
+    FsComponentComponent,
+  ],
+  providers: [
+    // FsComponentService,
+  ],
+})
+export class FsGalleryModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: FsGalleryModule,
+      // providers: [FsComponentService]
+    };
+  }
+}
