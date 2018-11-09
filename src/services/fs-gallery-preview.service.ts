2abdc257e02b36816dc9c51c82aa9d8c28cfdc18
Anatoly <mendor1990@gmail.com>
2018-11-09 12:27:42 +0200

    fixed build error

--- src/services/fs-gallery-preview.service.ts
+++ src/services/fs-gallery-preview.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
 
 import { FsGalleryDataItem } from '../interfaces';
 
