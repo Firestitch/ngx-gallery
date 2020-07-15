0807f2007df499ac10ac3340049134b345715a19
Eugene Kubesh <e.kubesh@wis.eu.com>
2020-07-15 20:54:27 +0300

    add injectable decorator to hammer config

--- src/app/classes/hammer-config.ts
+++ src/app/classes/hammer-config.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@angular/core';
 import { HammerGestureConfig } from '@angular/platform-browser';
 
 
+@Injectable()
 export class HammerConfig extends HammerGestureConfig  {
   overrides = <any>{
       swipe: { velocity: 0.4, threshold: 20 }
