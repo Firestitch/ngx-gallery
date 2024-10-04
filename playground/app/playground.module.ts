2ec791a10a5501dba0adda0e960024e44da0afed
Deployment Manager <sysadmin@firestitch.com>
2024-10-04 12:40:05 +0000

    Angular 15 Upgrade

--- playground/app/playground.module.ts
+++ playground/app/playground.module.ts
@@ -55,7 +55,7 @@ const routes: Routes = [
         }),
         FsApiModule.forRoot(),
         FsMenuModule,
-        RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
+        RouterModule.forRoot(routes, {}),
         FsExampleModule.forRoot(),
         ToastrModule.forRoot({ preventDuplicates: true }),
         FsMessageModule.forRoot(),
